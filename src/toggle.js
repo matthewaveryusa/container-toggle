e9584cfa9185645913d2d75f7171b229526e6bdb
matthewaveryusa <matthewaveryusa@gmail.com>
2015-04-06 17:41:39 -0400

    depricated docker.io for dockerode to support iojs

--- src/toggle.js
+++ src/toggle.js
@@ -1,10 +1,9 @@
-#! /usr/local/bin/node
+#! /usr/local/bin/iojs
 'use strict';
 
-var docker,
-  Service = require('totem-service').Service,
-  commander = require('commander'),
-  api;
+const Service = require('totem-service').Service,
+  commander = require('commander');
+
 
 commander.version('0.0.1')
   .option('--port [80]','port to bind to',80)
@@ -13,49 +12,30 @@ commander.version('0.0.1')
   .option('--docker-socket <path>','path to docker socket', '/var/run/docker.sock')
   .parse(process.argv);
 
-docker = require('docker.io')({ socketPath: commander.dockerSocket });
+const docker = new require('dockerode')({ socketPath: commander.dockerSocket });
 
-function Api(options) {
-  Service.call(this,options);
-};
-Api.prototype = Object.create(Service.prototype);
-Api.prototype.constructor = Api;
-Api.prototype.setupRoutes = function(app) {
-  var self = this;
-  app.get('/start', function(req, res) { 
-    docker.containers.start(self.options.container, {}, function(err,data){
+class Api extends Service {
+  constructor(options) {
+    super(options);
+  }
+
+  setupRoutes(app) {
+  const self = this,
+        container = docker.getContainer(self.options.container);
+  app.get('/:action', function(req, res) { 
+    if(!container[req.params.action]) {
+     return res.status(404).end();
+    }
+    container[req.params.action](function(err,data){
       if(err) {
+        console.log(err);
         res.status(500).end();
       } else 
         res.status(200).end();
       });
   });
-  
-  app.get('/stop', function(req, res) {
-    docker.containers.stop(self.options.container, {}, function(err,data){
-      if(err) {
-        res.status(500).end();
-      } else 
-        res.status(200).end();
-      });
-  });
-  app.get('/pause', function(req, res) { 
-    docker.containers.pause(self.options.container, {}, function(err,data){
-      if(err) {
-        res.status(500).end();
-      } else 
-        res.status(200).end();
-      });
-  });
-  
-  app.get('/unpause', function(req, res) {
-    docker.containers.unpause(self.options.container, {}, function(err,data){
-      if(err) {
-        res.status(500).end();
-      } else 
-        res.status(200).end();
-      });
-  });
-};
-api = new Api({'port':commander.port, 'host':commander.host, 'container':commander.container});
+  }
+}
+
+const api = new Api({'port':commander.port, 'host':commander.host, 'container':commander.container});
 api.connect();
